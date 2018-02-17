23c2e9791984008e8549a12fe56d60937b66def5
Dimmi <pechunka11@gmail.com>
2018-02-17 15:51:55 +0100

    tried fixed prod issue

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 //prod.js - production keys here!
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOCIE_KEY
+  googleClientID: process.ENV.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.ENV.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.ENV.MONGO_URI,
+  cookieKey: process.ENV.COOCIE_KEY
 };
