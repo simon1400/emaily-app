60255d4b4ec90e32911bceb4481943f3f3577f47
Dimmi <pechunka11@gmail.com>
2018-02-24 23:33:24 +0100

    fixed bug in root file

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 if(process.env.NODE_ENV === 'production'){
+  const path = require('path');
   // Express will serve up production assets
   // like our main.js file, or main.css file!
   app.use(express.static('client/bulid'));
 
   // Express will serve up the index.html file
   // if it doesent recognize the route
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
