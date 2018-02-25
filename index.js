0e971d1a0ac49b842ecd1ae5c1b4fd8583d38ff1
Dimmi <pechunka11@gmail.com>
2018-02-25 20:58:47 +0100

    Mongoose for Survey Creation done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -24,7 +25,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
-
+require('./routes/surveyRoutes')(app);
 
 if(process.env.NODE_ENV === 'production'){
     const path = require('path'); //We need path earlier for this!
